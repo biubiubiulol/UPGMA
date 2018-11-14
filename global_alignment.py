0bc6a9138bb415596dd9ea92d2e6878f7085c50b
ShabanTSU <shabanagayev@yahoo.com>
2018-11-14 17:24:48 -0600

    Finished backtrace

--- global_alignment.py
+++ global_alignment.py
@@ -41,40 +41,45 @@ def align():
 
             scores[i][j] = max(val1, val2, val3)
 
-    print(scores)
-
-
-'''
     seq1_align = ''
     seq2_align = ''
 
     i = s1_length
     j = s2_length
 
-    print(scores)
-    print("final")
-    while i > 1 and j > 1:
-        if scores[i][j] == ((scores[i-1][j-1]) - match_bonus) or ((scores[i-1][j-1]) - mismatch_penalty):
+    while i > 0 and j > 0:
+        if (scores[i][j]) == ((scores[i-1][j-1]) + match_bonus):
             seq1_align += s1[i-1]
             seq2_align += s2[j-1]
             i -= 1
             j -= 1
-        elif scores[i][j] == (scores[i][j-1] - gap_penalty):
-            seq1_align += s1[i-1]
+        elif scores[i][j] == ((scores[i][j-1]) + gap_penalty):
+            seq1_align += s2[j-1]
             seq2_align += "-"
             j -= 1
-        elif scores[i][j] == (scores[i-1][j] - gap_penalty):
+        elif scores[i][j] == ((scores[i-1][j]) + gap_penalty):
             seq1_align += "-"
-            seq2_align += s2[j-1]
+            seq2_align += s1[i-1]
             i -= 1
-        else:
-            print("error")
-        print(scores)
-        print(seq1_align)
-        print(seq2_align)
+        elif scores[i][j] == ((scores[i - 1][j - 1]) + mismatch_penalty):
+            seq1_align += s2[j-1]
+            seq2_align += s1[i-1]
+            i -= 1
+            j -= 1
+
+    while i != 0:
+        seq1_align += '-'
+        seq2_align += s1[i-1]
+        i -= 1
+
+    while j != 0:
+        seq2_align += '-'
+        seq1_align += s2[j - 1]
+        j -= 1
 
     print(scores)
-'''
+    print(seq1_align[::-1])
+    print(seq2_align[::-1])
 
 
 def main():
