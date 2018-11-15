9c1cd1995a50254a5c3c4ae324cf167904f7bff0
djwagner42 <will@bordowitz.com>
2018-11-14 18:34:40 -0600

    Update distance.py

--- distance.py
+++ distance.py
@@ -3,7 +3,6 @@ Finds distance using Kimura's 2-Parameter metric
 (Kimura, 1980)
 '''
 import math
-import global_alignment as ga
 
 #TODO: Alignment of the 2 sequences
 
@@ -15,7 +14,7 @@ def align(seq1, seq2):
 
 
 def k2p(sequence_1, sequence_2):
-    seq_list =  ga.align(sequence_1, sequence_2)
+    seq_list =  align(sequence_1, sequence_2)
     seq1 = seq_list[1]
     seq2 = seq_list[2]
     alpha = 0.9
@@ -31,6 +30,7 @@ def k2p(sequence_1, sequence_2):
         "G" : 0,
         "C" : 1,
         "T" : 1,
+        "-" : -1,
     }
     number_transitions = 0
     number_transversions = 0
@@ -38,10 +38,11 @@ def k2p(sequence_1, sequence_2):
         type1 = bases.get(seq1[base])
         type2 = bases.get(seq2[base])
         if seq1[base] != seq2[base]:
-            if type1 == type2:
-                number_transitions = number_transitions+1
-            else:
-                number_transversions = number_transversions+1
+            if type1 != -1 or type2 != -1:
+                if type1 == type2:
+                    number_transitions = number_transitions+1
+                elif type1 != type2:
+                    number_transversions = number_transversions+1
     s = number_transitions/length
     v = number_transversions/length
     d = -(.5)*math.log(1- 2*s - v) - (0.25)*math.log(1- 2*v) 
